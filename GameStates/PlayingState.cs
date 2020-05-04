e1218b4d73f8c910847eaa5c6863c99c625c99fd
aslokaa <aslokaa@gmail.com>
2020-05-04 21:01:57 +0200

    Actually added playingstate

---
+++ GameStates/PlayingState.cs
@@ -0,0 +1,38 @@
+﻿using Centipede.GameObjects;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Centipede.GameStates
+{
+    class PlayingState : GameObjectList
+    {
+        SpriteGameObject background = new SpriteGameObject("background");
+        Hero player = new Hero("");
+
+        public PlayingState()
+        {
+            Reset();
+            Init();
+        }
+
+        public override void Reset()
+        {
+            base.Reset();
+        }
+
+        public void Init()
+        { 
+            this.Add();
+        }
+
+        public override void HandleInput(InputHelper inputHelper)
+        {
+            base.HandleInput(inputHelper);
+            
+        }
+    }
+}
